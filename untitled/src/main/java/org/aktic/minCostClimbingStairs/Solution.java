3c88a341889e2f56696038d6c95f6168d1898d60
Azizullah Khan <aziz.bin.aman16@gmail.com>
2024-10-05 22:03:09 -0700

    new questions done

--- untitled/src/main/java/org/aktic/minCostClimbingStairs/Solution.java
+++ untitled/src/main/java/org/aktic/minCostClimbingStairs/Solution.java
@@ -2,22 +2,22 @@ package org.aktic.minCostClimbingStairs;
 
 public class Solution {
     public static int minCostClimbingStairs(int[] cost) {
-        if(cost.length < 3) return 0;
+        if(cost.length < 3) return Math.min(cost[0], cost[1]);
 
-        int[] dp = new int[cost.length];
+        int[] dp = new int[cost.length+1];
 
         dp[0] = 0;
         dp[1] = 0;
 
-        for (int i = 2; i < cost.length; i++) {
-            dp[i] = Math.min(cost[i-1], cost[i-2]);
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(cost[i-1]+dp[i-1], cost[i-2]+dp[i-2]);
         }
 
-        return Math.min(dp[cost.length-1], dp[cost.length-2]);
+        return dp[dp.length-1];
     }
 
     public static void main(String[] args) {
-        int[] cost = {10,15,20};
+        int[] cost = {1,100,1,1,1,100,1,1,100,1};
         System.out.println(minCostClimbingStairs(cost));
     }
 }
