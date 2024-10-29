28076121e2bf82e2de33816ad0a35e2daff12957
Azizullah Khan <aziz.bin.aman16@gmail.com>
2024-10-29 11:28:24 +0500

    new questions

---
+++ problems/src/main/java/org/aktic/eraseOverlapIntervals/Solution.java
@@ -0,0 +1,15 @@
+package org.aktic.eraseOverlapIntervals;
+
+public class Solution {
+    public static int eraseOverlapIntervals(int[][] intervals) {
+        int min = 0;
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {{1,2},{2,3},{3,4},{1,3}};
+
+        System.out.println(eraseOverlapIntervals(array));
+    }
+}
---
+++ problems/src/main/java/org/aktic/reverseList/Solution.java
@@ -0,0 +1,48 @@
+package org.aktic.reverseList;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    @Override
+    public String toString() {
+        return "ListNode [val=" + val + ", next=" + next + "]";
+    }
+}
+
+public class Solution {
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode fifth = new ListNode(5);
+        ListNode fourth = new ListNode(4, fifth);
+        ListNode third = new ListNode(3, fourth);
+        ListNode second = new ListNode(2, third);
+        ListNode first = new ListNode(1, second);
+
+        reverseList(first);
+
+        ListNode reversed = reverseList(first);
+
+        while (reversed != null) {
+            System.out.println(reversed);
+            reversed = reversed.next;
+        }
+
+    }
+}
