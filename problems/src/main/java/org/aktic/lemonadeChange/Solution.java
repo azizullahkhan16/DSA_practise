764532b2ca55ebebb1c59554f858e13628a14be7
Azizullah Khan <aziz.bin.aman16@gmail.com>
2024-12-31 16:05:46 +0500

    added new question

--- problems/src/main/java/org/aktic/lemonadeChange/Solution.java
+++ problems/src/main/java/org/aktic/lemonadeChange/Solution.java
@@ -2,10 +2,38 @@ package org.aktic.lemonadeChange;
 
 public class Solution {
     public static boolean lemonadeChange(int[] bills) {
-        return false;
+        int num5 = 0;
+        int num10 = 0;
+        int num20 = 0;
+
+        for (int i = 0; i < bills.length; i++) {
+            int change = bills[i]-5;
+            if(change > 0){
+                if(num20 > 0 && num20 >= change/20) {
+                    num20 -= change/20;
+                    change -= 20*(change/20);
+                }
+                if(num10 > 0 && num10 >= change/10){
+                    num10 -= change/10;
+                    change -= 10*(change/10);
+                }
+                if(num5 > 0 && num5 >= change/5){
+                    num5 -= change/5;
+                    change -= 5*(change/5);
+                }
+
+                if(change != 0) return false;
+            }
+
+            if(bills[i] == 5) num5++;
+            else if(bills[i] == 10) num10++;
+            else if(bills[i] == 20) num20++;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(lemonadeChange(new int[]{5,5,5,10,20}));
     }
 }
