a3e1ca75d52a349a8b43ad858fbd0dab037c8aad
Azizullah Khan <aziz.bin.aman16@gmail.com>
2025-01-22 15:44:27 +0500

    added new questions

--- problems/src/main/java/org/aktic/gridGame/Solution.java
+++ problems/src/main/java/org/aktic/gridGame/Solution.java
@@ -1,13 +1,36 @@
 package org.aktic.gridGame;
 
 public class Solution {
-    public long gridGame(int[][] grid) {
-        long row1Sum = 0;
-        long row2Sum = 0;
+    public static long gridGame(int[][] grid) {
+        int n = grid[0].length;
+        long[] prefixTop = new long[n];
+        long[] prefixBottom = new long[n];
 
-        for(int i = 0; i < grid[0].length; i++) {
-            row1Sum += grid[0][i];
-            row2Sum += grid[1][i];
+        // Compute prefix sums for both rows
+        prefixTop[0] = grid[0][0];
+        prefixBottom[0] = grid[1][0];
+        for (int i = 1; i < n; i++) {
+            prefixTop[i] = prefixTop[i - 1] + grid[0][i];
+            prefixBottom[i] = prefixBottom[i - 1] + grid[1][i];
         }
+
+        long minSecondRobotScore = Long.MAX_VALUE;
+
+        // Simulate the first robot's path and calculate the second robot's maximum score
+        for (int i = 0; i < n; i++) {
+            // Remaining sums for the second robot
+            long topRemaining = (i == n - 1) ? 0 : prefixTop[n - 1] - prefixTop[i];
+            long bottomRemaining = (i == 0) ? 0 : prefixBottom[i - 1];
+
+            long secondRobotScore = Math.max(topRemaining, bottomRemaining);
+            minSecondRobotScore = Math.min(minSecondRobotScore, secondRobotScore);
+        }
+
+        return minSecondRobotScore;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gridGame(new int[][] {{2,5,4},
+                {1,5,1}}));
     }
 }
