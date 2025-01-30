6d5f6a476f947f9c404b6bdb6b73d9b8ad4faf1c
Azizullah Khan <aziz.bin.aman16@gmail.com>
2025-01-30 21:17:28 +0500

    added new question

---
+++ problems/src/main/java/org/aktic/getSum/Solution.java
@@ -0,0 +1,17 @@
+package org.aktic.getSum;
+
+public class Solution {
+    public static int getSum(int a, int b) {
+        while(b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
