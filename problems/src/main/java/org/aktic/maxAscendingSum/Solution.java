595c8f4dd2349e577bca7a44b5c631a7c9d6ca9a
Azizullah Khan <aziz.bin.aman16@gmail.com>
2025-02-04 15:04:28 +0500

    added new question

---
+++ problems/src/main/java/org/aktic/maxAscendingSum/Solution.java
@@ -0,0 +1,26 @@
+package org.aktic.maxAscendingSum;
+
+public class Solution {
+    public static int maxAscendingSum(int[] nums) {
+        int max = nums[0];
+        int curr = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                curr = nums[i];
+            } else {
+                curr += nums[i];
+            }
+
+            max = Math.max(max, curr);
+        }
+
+
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxAscendingSum(new int[]{10, 20, 30, 5, 10, 50}));
+    }
+}
