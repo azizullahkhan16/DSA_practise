96a4fd6526e62d94e3777940fd86dcf96fbbff8f
Azizullah Khan <aziz.bin.aman16@gmail.com>
2025-05-21 11:50:02 +0500

    added new question

--- problems/src/main/java/org/aktic/setZeroes/Solution.java
+++ problems/src/main/java/org/aktic/setZeroes/Solution.java
@@ -3,13 +3,45 @@ package org.aktic.setZeroes;
 import java.util.Arrays;
 
 public class Solution {
+    //    public static void setZeroes(int[][] matrix) {
+//        boolean[] rowZero = new boolean[matrix.length];
+//        boolean[] colZero = new boolean[matrix[0].length];
+//
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                if(matrix[i][j] == 0) {
+//                    rowZero[i] = true;
+//                    colZero[j] = true;
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < rowZero.length; i++) {
+//            if(rowZero[i]) setRowZeroes(matrix, i);
+//        }
+//
+//        for (int i = 0; i < colZero.length; i++) {
+//            if(colZero[i]) setColZeroes(matrix, i);
+//        }
+//    }
+//
+//    public static void setRowZeroes(int[][] matrix, int row) {
+//        Arrays.fill(matrix[row], 0);
+//    }
+//
+//    public static void setColZeroes(int[][] matrix, int col) {
+//        for (int i = 0; i < matrix.length; i++) {
+//            matrix[i][col] = 0;
+//        }
+//
+//    }
     public static void setZeroes(int[][] matrix) {
         boolean[] rowZero = new boolean[matrix.length];
         boolean[] colZero = new boolean[matrix[0].length];
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j] == 0) {
+                if (matrix[i][j] == 0) {
                     rowZero[i] = true;
                     colZero[j] = true;
                 }
@@ -17,26 +49,31 @@ public class Solution {
         }
 
         for (int i = 0; i < rowZero.length; i++) {
-            if(rowZero[i]) setRowZeroes(matrix, i);
+            if (rowZero[i]) {
+                makeRowZero(matrix, i);
+            }
         }
 
         for (int i = 0; i < colZero.length; i++) {
-            if(colZero[i]) setColZeroes(matrix, i);
+            if (colZero[i]) {
+                makeColZero(matrix, i);
+            }
         }
     }
 
-    public static void setRowZeroes(int[][] matrix, int row) {
-        Arrays.fill(matrix[row], 0);
+    public static void makeRowZero(int[][] matrix, int row) {
+        for (int i = 0; i < matrix[0].length; i++) {
+            matrix[row][i] = 0;
+        }
     }
 
-    public static void setColZeroes(int[][] matrix, int col) {
+    public static void makeColZero(int[][] matrix, int col) {
         for (int i = 0; i < matrix.length; i++) {
             matrix[i][col] = 0;
         }
-
     }
 
     public static void main(String[] args) {
-        setZeroes(new int[][] {{1,1,1},{1,0,1},{1,1,1}});
+        setZeroes(new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}});
     }
 }
