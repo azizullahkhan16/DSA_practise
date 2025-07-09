377ccbd95f4fde2886f53d80362da16cf46aeddf
Azizullah Khan <aziz.bin.aman16@gmail.com>
2025-07-09 18:06:11 +0500

    added new question

---
+++ problems/src/main/java/org/aktic/maxFreeTime/Solution.java
@@ -0,0 +1,37 @@
+package org.aktic.maxFreeTime;
+
+public class Solution {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int count = startTime.length;
+        int max = 0;
+        int[] prefixSum = new int[count + 1];
+
+        for (int i = 0; i < count; i++) {
+            prefixSum[i + 1] = prefixSum[i] + endTime[i] - startTime[i];
+        }
+
+        for (int i = k - 1; i < count; i++) {
+            int occupied = prefixSum[i + 1] - prefixSum[i - k + 1];
+            int windowEnd = (i == count - 1) ? eventTime : startTime[i + 1];
+            int windowStart = (i == k - 1) ? 0 : endTime[i - k];
+            max = Math.max(max, windowEnd - windowStart - occupied);
+        }
+
+        return max;
+    }
+
+
+    private static int countMax(int[] startTime, int[] endTime) {
+        int max = startTime[0];
+        int left = 1;
+        int right = 0;
+
+        while (left < startTime.length) {
+            max = Math.max(max, startTime[left] - endTime[right]);
+            left++;
+            right++;
+        }
+
+        return max;
+    }
+}
